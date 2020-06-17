8aa4bd273d9a2ffcbc09e303df66d46fef29721f
Pratik <Pratikshaha339@gmail.com>
2020-06-17 23:05:08 +0000

    Clang format code

--- src/graphedge.cpp
+++ src/graphedge.cpp
@@ -1,29 +1,19 @@
-#include "graphnode.h"
 #include "graphedge.h"
+#include "graphnode.h"
 
 #include <iostream>
 
-GraphEdge::GraphEdge(int id)
-{
-    std::cout << "GraphEdge constructor\n";
-    _id = id;
+GraphEdge::GraphEdge(int id) {
+  std::cout << "GraphEdge constructor\n";
+  _id = id;
 }
 
-GraphEdge::~GraphEdge() {
-    std::cout << "GraphEdge deconstructor\n";
+GraphEdge::~GraphEdge() { std::cout << "GraphEdge deconstructor\n"; }
+
+void GraphEdge::SetChildNode(GraphNode *childNode) { _childNode = childNode; }
+
+void GraphEdge::SetParentNode(GraphNode *parentNode) {
+  _parentNode = parentNode;
 }
 
-void GraphEdge::SetChildNode(GraphNode *childNode)
-{
-    _childNode = childNode;
-}
-
-void GraphEdge::SetParentNode(GraphNode *parentNode)
-{
-    _parentNode = parentNode;
-}
-
-void GraphEdge::AddToken(std::string token)
-{
-    _keywords.push_back(token);
-}
+void GraphEdge::AddToken(std::string token) { _keywords.push_back(token); }
